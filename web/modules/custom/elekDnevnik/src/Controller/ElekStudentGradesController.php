3a3f4c84cb702cf76579134a95aa07ec657da1ce
stojk3 <stojkebrd@gmail.com>
2024-11-01 10:16:30 +0100

    Izvestaj za ocene sada izlistava ocene ali sve. Popravice se

--- web/modules/custom/elekDnevnik/src/Controller/ElekStudentGradesController.php
+++ web/modules/custom/elekDnevnik/src/Controller/ElekStudentGradesController.php
@@ -3,61 +3,74 @@
 namespace Drupal\elekDnevnik\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Request;
 use Drupal\Core\Database\Database;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class ElekStudentGradesController extends ControllerBase {
 
-  public function gradesPage(Request $request) {
-    $current_user = \Drupal::currentUser();
-    $user_id = $current_user->id();
+    protected $currentUser;
 
-    $connection = Database::getConnection();
-    $query = $connection->select('user_registration', 'ur')
-      ->fields('ur', ['first_name', 'last_name'])
-      ->condition('id', $user_id)
-      ->execute()
-      ->fetchObject();
-
-    if (!$query) {
-      return [
-        '#markup' => $this->t('Student data not found.'),
-      ];
+    public function __construct(AccountInterface $current_user) {
+        $this->currentUser = $current_user;
     }
 
-    $student_name = $query->first_name . ' ' . $query->last_name;
-
-    $grades_query = $connection->select('student_grades', 'sg')
-      ->fields('sg', ['naziv_predmeta', 'ocena'])
-      ->condition('student_id', $user_id)
-      ->execute();
-
-    $grades_data = [];
-    foreach ($grades_query as $record) {
-      $subject = str_replace('_', ' ', ucfirst($record->naziv_predmeta)); // format subject name
-      $grades_data[$subject][] = $record->ocena;
+    public static function create(ContainerInterface $container) {
+        return new static(
+            $container->get('current_user')
+        );
     }
 
-    $grades_display = [];
-    foreach ($grades_data as $subject => $grades) {
-      $average = array_sum($grades) / count($grades);
-      $grades_display[] = [
-        'subject' => $subject,
-        'grades' => implode(', ', $grades),
-        'average' => number_format($average, 2),
-      ];
+    public function viewGrades() {
+        $user_id = $this->currentUser->id();
+        $roles = $this->currentUser->getRoles();
+
+        $query = Database::getConnection()->select('student_grades', 'g');
+        $query->fields('g', ['naziv_predmeta', 'ocena']);
+        
+        if (in_array('Ucenik', $roles)) {
+            $query->condition('student_id', $user_id);
+        }
+
+        $results = $query->execute()->fetchAll();
+
+        if (empty($results)) {
+            \Drupal::logger('elekDnevnik')->info('Nema rezultata za korisnika ID: @user_id', ['@user_id' => $user_id]);
+        }
+
+        $grades_by_subject = [];
+        foreach ($results as $result) {
+            $subject = $result->naziv_predmeta;
+            if (!isset($grades_by_subject[$subject])) {
+                $grades_by_subject[$subject] = [];
+            }
+            $grades_by_subject[$subject][] = $result->ocena;
+        }
+
+        $rows = [];
+        foreach ($grades_by_subject as $subject => $grades) {
+            $grades_string = implode(', ', $grades);
+            $average_grade = array_sum($grades) / count($grades);
+            $rows[] = [
+                'subject' => ucfirst(str_replace('_', ' ', $subject)),
+                'grades' => $grades_string,
+                'average' => number_format($average_grade, 2),
+            ];
+        }
+
+        $header = [
+            'subject' => $this->t('Predmet'),
+            'grades' => $this->t('Ocene'),
+            'average' => $this->t('Prosečna ocena'),
+        ];
+
+        $build = [
+            '#type' => 'table',
+            '#header' => $header,
+            '#rows' => $rows,
+            '#empty' => $this->t('Nemate zabeležene ocene.'),
+        ];
+
+        return $build;
     }
-
-    return [
-      '#theme' => 'student_grades_report',
-      '#student_name' => $student_name,
-      '#grades_display' => $grades_display,
-      '#attached' => [
-        'library' => [
-          'elekDnevnik/student_grades',
-        ],
-      ],
-    ];
-  }
-
 }
